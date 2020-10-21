78be2fdc4eb613d952111a44e84cf9aa5c04f12d
blueedgetechno <blueedgetechno@gmail.com>
2020-10-21 10:54:30 +0530

    two

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const middlewares = require('./middlewares');
-const api = require('./api');
+const api = require(__dirname + '/api');
 
 const app = express();
 
